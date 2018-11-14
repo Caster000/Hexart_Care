18c71ae9e01c7ebe9b86fbab1c5274e5359a94fa
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-14 23:12:11 +0100

    Update menu.c

--- Module4/menu.c
+++ Module4/menu.c
@@ -3,17 +3,19 @@
 //
 #include <stdio.h>
 #include "menu.h"
-void menu(){
-    int choix;
+void menu(int choix){
+
     printf("---Menu---");
     printf("1. Afficher les donnees obtenu");
-    printf("2. Afficher les donnees par ordre croissant");
-    printf("3. Afficher les donnees par ordre decroissant");
-    printf("4. Afficher la frequence cardiaque pour un temps t");
-    printf("5. Afficher la moyenne de la frequence cardiaque entre un instant t1 et t2");
-    printf("6. Afficher le nombre de ligne en mémoire");
-    printf("7. Afficher la frequence cardiaque maximum");
-    printf("8. Afficher la frequence cardiaque minimum");    // +++ Temps associé
+    printf("2. Afficher les donnees par ordre croissant selon le temps");
+    printf("3. Afficher les donnees par ordre croissant selon le pouls");
+    printf("4. Afficher les donnees par ordre decroissant selon le temps");
+    printf("5. Afficher les donnees par ordre decroissant selon le pouls");
+    printf("6. Afficher la frequence cardiaque pour un temps t");
+    printf("7. Afficher la moyenne de la frequence cardiaque entre un instant t1 et t2");
+    printf("8. Afficher le nombre de ligne en mémoire");
+    printf("9. Afficher la frequence cardiaque maximum");
+    printf("10. Afficher la frequence cardiaque minimum");    // +++ Temps associé
     printf("0. Quitter");
     scanf("%d",choix);
     switch(choix){
@@ -21,24 +23,31 @@ void menu(){
             donneesBrut();
             break;
         case 2:
-            ordreCroissant();
+            ordreCroissantTemps();
             break;
         case 3:
-            ordreDecroissant();
+            ordreCroissantPouls();
             break;
         case 4:
-            rechercheFrequence();
+            ordreDecroissantTemps();
             break;
         case 5:
-            moyenne();
+            ordreDecroissantPouls();
             break;
         case 6:
-            nombreLigne();
+            rechercheFrequence();
+
             break;
         case 7:
-            max();
+            moyenne();
             break;
         case 8:
+            nombreLigne();
+            break;
+        case 9:
+            max();
+            break;
+        case 10:
             mini();
             break;
         case 0:
@@ -46,8 +55,5 @@ void menu(){
         default:
             printf("Erreur, veuillez choisir une option valide");
             break;
-
-
-
     }
 }
