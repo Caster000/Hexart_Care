fa10d4fdaf288d3787c8eaf4cebc2a7019284f5e
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-14 23:11:44 +0100

    Update main.c

--- Module4/main.c
+++ Module4/main.c
@@ -1,6 +1,41 @@
+#include <string.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "menu.h"
+#include "donnees.h"
+#include "actions.h"
+#
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+int i;
+
+    Donnees *tab = NULL;                //creation de ,notre tableau de structure
+
+    FILE* fichier = NULL;
+
+    if((fichier = fopen("info.csv", "r")) == NULL) {                    //charger info.csv
+        printf("\n---Impossible d'ouvrir -> info.csv.---\n");           //message d'erreur si pas de fichier
+        exit(-1);
+    }
+    unsigned  int nb_line;
+
+
+                         //compte le nb de ligne
+  int c;                                                   //sert à regarder chaque caracère du fichier
+  FILE *fp;                                               // j'ouvre le fichier que je désire en mode "r"
+  while ((c = getc(fp)) != EOF){
+      if (c == '\n')                                        // \n correspond a un char, donc à un entier.
+          ++nb_line;
+  }                                                                      //fin compte ligne
+    rewind(fichier);//remise au cursuer au début
+
+
+    //allocation du tableau de donnée avec une taille dynamique selon nb_line
+    if((tab = malloc(nb_line * sizeof(Donnees))) == NULL)
+    {
+        printf("\n---Unable to allocate space for tableau_data.---\n");
+        exit(-1);
+    }
+    ecritureCSV(fichier,nb_line, tab);
+    menu(choix);
+  return 0;
 }
