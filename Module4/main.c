dd8df82ad6f1499769e2b8fbe41c7d7d1f06d10d
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-15 11:33:31 +0100

    Update main.c

--- Module4/main.c
+++ Module4/main.c
@@ -4,29 +4,29 @@
 #include "menu.h"
 #include "donnees.h"
 #include "actions.h"
-#
-int main() {
-int i;
 
-    Donnees *tab = NULL;                //creation de ,notre tableau de structure
+int main() {
+
+    unsigned  int nb_line;
+    Donnees *tab = NULL;                //creation de notre tableau de structure
 
     FILE* fichier = NULL;
 
-    if((fichier = fopen("info.csv", "r")) == NULL) {                    //charger info.csv
+    if((fichier = fopen("info.csv", "r")) == NULL) {                    //tente de charger info.csv
         printf("\n---Impossible d'ouvrir -> info.csv.---\n");           //message d'erreur si pas de fichier
         exit(-1);
     }
-    unsigned  int nb_line;
+
 
 
                          //compte le nb de ligne
   int c;                                                   //sert à regarder chaque caracère du fichier
-  FILE *fp;                                               // j'ouvre le fichier que je désire en mode "r"
-  while ((c = getc(fp)) != EOF){
+                                              // j'ouvre le fichier que je désire en mode "r"
+  while ((c = fgetc(fichier)) != EOF){
       if (c == '\n')                                        // \n correspond a un char, donc à un entier.
           ++nb_line;
   }                                                                      //fin compte ligne
-    rewind(fichier);//remise au cursuer au début
+    rewind(fichier);//remise au curseur au début
 
 
     //allocation du tableau de donnée avec une taille dynamique selon nb_line
@@ -36,6 +36,3 @@ int i;
         exit(-1);
     }
     ecritureCSV(fichier,nb_line, tab);
-    menu(choix);
-  return 0;
-}
