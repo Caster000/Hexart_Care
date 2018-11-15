1b26b5ce265b456e8a3e91a32643516f83132624
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-15 12:55:19 +0100

    Update actions.c

--- Module4/actions.c
+++ Module4/actions.c
@@ -1,10 +1,20 @@
 //
 // Created by cheva on 13/11/2018.
 //
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "actions.h"
+#include "donnees.h"
 
-void donneesBrut(){
+
+
+
+void donneesBrut( int nb_line, struct Donnees *tab){
+     int i;
+    for (i=0;i<nb_line;i++){
+        printf("%d;%d", tab[i].milli, tab[i].pouls);
+    }
 
 }
 
