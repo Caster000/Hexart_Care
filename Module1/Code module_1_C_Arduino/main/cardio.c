d81140ee07a42f7019da370a572d8e47a5f6458e
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-15 10:27:21 +0100

    Add files via upload

--- Module1/Code module_1_C_Arduino/main/cardio.c
+++ Module1/Code module_1_C_Arduino/main/cardio.c
@@ -3,20 +3,21 @@
 
 unsigned long time;
 
-int temps(){
-    time = millis();
+int temps(){                      //calcul le temps total du programme
+  
+    ++time;
     return(time);
   }
 
-int calculBattement(int battement){ 
+int calculBattement(int battement){                   //incrementation pour le nombre de battement
     battement++;
-    delay(70);
+    delay(70);                                        //delai symbolisant la fin de la variation d'un battement
     return(battement);
   }
 
 
-int calculPouls(int battement){
-     int pouls = 4*battement;
+int calculPouls(int battement){                       
+     int pouls = 4*battement;                         //on calcul pour avoir le pouls par minute
      return(pouls);
   }
 
--- Module1/Code module_1_C_Arduino/main/main.ino
+++ Module1/Code module_1_C_Arduino/main/main.ino
@@ -1,33 +1,36 @@
 #include "cardio.c"
 #include "cardio.h"
-long t;
-int compteur = 0;
+//#include "coeur.c"
+//#include "coeur.h"
+
+long t;                           //temps
+int compteur1 = 0;                 //periodicité mesure
 int battement = 0;
 int pouls = 0;
 
 void setup()
 {
   pinMode(A0, INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600);           
 }
 
 void loop()
 {
-  compteur++;
+  compteur1++;
 
-  if(analogRead(0) >= 60){
-    battement = calculBattement(battement);
+  if(analogRead(0) >= 60){                          // quand on a une variation >=60
+    battement = calculBattement(battement);         //on compte les battements
   }
 
-  t = temps();
+  t = temps();                                      //fonction temps total
 
-  if(compteur == 150){ 
-    pouls = calculPouls(battement);
-    Serial.print(t);
-    Serial.print(";");
-    Serial.println(pouls);
-    battement = 0;
-    compteur = 0;
+  if(compteur == 1000){                              //toutes les 1000 loop on affiche les valeurs
+    pouls = calculPouls(battement);               
+    Serial.print(t);                               //affiche temps
+    Serial.print(";");                            
+    Serial.print(pouls);                        //affiche pouls
+    battement = 0;                               // renitialisation variables pour la prochaine boucle
+    compteur1 = 0;
   }
-  delay(10);
+  delay(1);                                    //attente entre chaque loop qui symbolise le temps
 }
