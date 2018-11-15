e8312c30f8e853ab8b2e5785c74b77a8912e0023
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-15 20:34:03 +0100

    Update menu.c

--- Module4/menu.c
+++ Module4/menu.c
@@ -1,14 +1,12 @@
-//
-// Created by cheva on 13/11/2018.
-//
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "menu.h"
-void menu(int choix, int nb_line, struct Donnees *tab){
 
-    printf("---Menu---");
-    printf("1. Afficher les donnees obtenu");
+void menu(int choix, int nb_line, struct Donnees *tab){
+//---------------------------------------------Affichage du menu----------------------------------------------------
+    printf("---Menu---\n");
+    printf("1. Afficher les donnees obtenu\n");
     printf("2. Afficher les donnees par ordre croissant selon le temps\n");
     printf("3. Afficher les donnees par ordre croissant selon le pouls\n");
     printf("4. Afficher les donnees par ordre decroissant selon le temps\n");
@@ -17,44 +15,55 @@ void menu(int choix, int nb_line, struct Donnees *tab){
     printf("7. Afficher la moyenne de la frequence cardiaque entre un instant t1 et t2\n");
     printf("8. Afficher le nombre de ligne en mémoire\n");
     printf("9. Afficher la frequence cardiaque maximum\n");
-    printf("10. Afficher la frequence cardiaque minimum\n");    // +++ Temps associé
+    printf("10. Afficher la frequence cardiaque minimum\n");
     printf("0. Quitter\n");
-    scanf("%d",&choix);
-    switch(choix){
+    scanf("%d", &choix);                                                 //demande du choix
+    int frequence = 0;
+    int borne1 = 0;
+    int borne2 = 0;
+    switch (choix) {
         case 1:
-            donneesBrut(nb_line, tab);
+            donneesBrut(nb_line, tab);                                  //afficher les donnees tel quel
             break;
         case 2:
-            ordreCroissantTemps();
+            ordreCroissantTemps(nb_line, tab);                          //afficher en ordre croissant selon temps
             break;
         case 3:
-            ordreCroissantPouls();
+            ordreCroissantPouls(nb_line, tab);                          //afficher en ordre croissant selon pouls
             break;
         case 4:
-            ordreDecroissantTemps();
+            ordreDecroissantTemps(nb_line, tab);                        //afficher en ordre decroissant selon temps
             break;
         case 5:
-            ordreDecroissantPouls();
+            ordreDecroissantPouls(nb_line, tab);                        //afficher en ordre decroissant selon pouls
             break;
         case 6:
-            rechercheFrequence();
+            printf("Pour quel temps voulez vous le pouls ?\nDonnez le temps en millisecondes. ex: \"1000\" ou \"2000\" \n"); //afficher frequence demandé
+            scanf("%d", &frequence);
+            rechercheFrequence(nb_line, tab, frequence);
             break;
         case 7:
-            moyenne(); //Ok
+            printf("Definir la premiere borne\nDonnez le temps en millisecondes, ex: \"1000\" ou \"2000\" \n");                      //afficher moyenne entre deux bornes
+            scanf(" %d", &borne1);
+            printf("Definir la seconde borne\nDonnez le temps en millisecondes, ex: \"1000\" ou \"2000\" \n");
+            scanf(" %d", &borne2);
+            moyenne(nb_line, tab, borne1, borne2);
             break;
         case 8:
-            nombreLigne();
+            printf("Le nombre de ligne est %d\n", nb_line);             //afficher nombre ligne
             break;
         case 9:
-            max();   //OK
+            max(nb_line, tab);                                          //afficher pouls maximum
             break;
         case 10:
-            mini();  //OK
+            mini(nb_line, tab);                                         //afficher pouls minimum
             break;
-        case 0:
+        case 0:                                                         //quitter
             exit(-1);
         default:
-            printf("Erreur, veuillez choisir une option valide");
+            printf("Erreur, veuillez choisir une option valide\n");
             break;
     }
+
 }
+
