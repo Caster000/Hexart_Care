a05e4a944a3612d83c5b528dee84238408ad7898
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-15 20:33:31 +0100

    Update main.c

--- Module4/main.c
+++ Module4/main.c
@@ -6,36 +6,38 @@
 #include "actions.h"
 
 int main() {
-    int choix;
-      int nb_line;
-   struct Donnees *tab;                //creation de notre tableau de structure
-
+    int choix=-1;
+      int nb_line=0;
+   struct Donnees *tab;                                      //creation de notre tableau de structure
+//------------------------------------------définition du fichier à utiliser-------------------------------------------
     FILE* fichier = NULL;
 
-    if((fichier = fopen("info.csv", "r")) == NULL) {                    //tente de charger info.csv
+    if((fichier = fopen("info.csv", "r")) == NULL) {                     //chargement de info.csv
         printf("\n---Impossible d'ouvrir -> info.csv.---\n");           //message d'erreur si pas de fichier
         exit(-1);
     }
 
-
-
-                         //compte le nb de ligne
-  int c;                                                   //sert à regarder chaque caracère du fichier
-                                              // j'ouvre le fichier que je désire en mode "r"
-  while ((c = fgetc(fichier)) != EOF){
+//--------------------------------------------------compte le nombre de ligne-------------------------------------------------------
+  int c;                                                    //sert à regarder chaque caracère du fichier
+  while ((c = fgetc(fichier)) != EOF){                      //EOF ---> End of File
       if (c == '\n')                                        // \n correspond a un char, donc à un entier.
           ++nb_line;
-  }                                                                      //fin compte ligne
-    rewind(fichier);//remise au curseur au début
+  } //----------------------------------fin compte ligne--------------------------------------
+    rewind(fichier);                                        //remise du curseur au début
 
 
-    //allocation du tableau de donnée avec une taille dynamique selon nb_line
+ //--------------------------------------allocation du tableau de donnée avec une taille dynamique selon nb_line------------------
     if((tab = malloc(nb_line * sizeof(struct Donnees))) == NULL)
     {
-        printf("\n---Unable to allocate space for tableau_data.---\n");
+        printf("\n---Unable to allocate space for tableau_data.---\n");    
         exit(-1);
     }
+//-----------------------------------utilisation des fonctions-------------------------------------------------------------
     ecritureCSV(fichier,nb_line, tab);
     menu(choix, nb_line, tab);
+
+
+    free(tab);                          //liberation memoire de malloc
+    fclose(fichier);                    //fermeture du fichier
   return 0;
 }
