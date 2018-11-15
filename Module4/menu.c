bb6a333415294cbb18ef3c7940128fb60edf385c
Caster000 <45029374+Caster000@users.noreply.github.com>
2018-11-15 12:54:05 +0100

    Update menu.c

--- Module4/menu.c
+++ Module4/menu.c
@@ -1,26 +1,28 @@
 //
 // Created by cheva on 13/11/2018.
 //
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "menu.h"
-void menu(int choix){
+void menu(int choix, int nb_line, struct Donnees *tab){
 
     printf("---Menu---");
     printf("1. Afficher les donnees obtenu");
-    printf("2. Afficher les donnees par ordre croissant selon le temps");
-    printf("3. Afficher les donnees par ordre croissant selon le pouls");
-    printf("4. Afficher les donnees par ordre decroissant selon le temps");
-    printf("5. Afficher les donnees par ordre decroissant selon le pouls");
-    printf("6. Afficher la frequence cardiaque pour un temps t");
-    printf("7. Afficher la moyenne de la frequence cardiaque entre un instant t1 et t2");
-    printf("8. Afficher le nombre de ligne en mémoire");
-    printf("9. Afficher la frequence cardiaque maximum");
-    printf("10. Afficher la frequence cardiaque minimum");    // +++ Temps associé
-    printf("0. Quitter");
-    scanf("%d",choix);
+    printf("2. Afficher les donnees par ordre croissant selon le temps\n");
+    printf("3. Afficher les donnees par ordre croissant selon le pouls\n");
+    printf("4. Afficher les donnees par ordre decroissant selon le temps\n");
+    printf("5. Afficher les donnees par ordre decroissant selon le pouls\n");
+    printf("6. Afficher la frequence cardiaque pour un temps t\n");
+    printf("7. Afficher la moyenne de la frequence cardiaque entre un instant t1 et t2\n");
+    printf("8. Afficher le nombre de ligne en mémoire\n");
+    printf("9. Afficher la frequence cardiaque maximum\n");
+    printf("10. Afficher la frequence cardiaque minimum\n");    // +++ Temps associé
+    printf("0. Quitter\n");
+    scanf("%d",&choix);
     switch(choix){
         case 1:
-            donneesBrut();
+            donneesBrut(nb_line, tab);
             break;
         case 2:
             ordreCroissantTemps();
@@ -36,22 +38,21 @@ void menu(int choix){
             break;
         case 6:
             rechercheFrequence();
-
             break;
         case 7:
-            moyenne();
+            moyenne(); //Ok
             break;
         case 8:
             nombreLigne();
             break;
         case 9:
-            max();
+            max();   //OK
             break;
         case 10:
-            mini();
+            mini();  //OK
             break;
         case 0:
-            return 0;
+            exit(-1);
         default:
             printf("Erreur, veuillez choisir une option valide");
             break;
